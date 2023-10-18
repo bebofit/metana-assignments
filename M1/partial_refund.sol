0ce41323750a84f310a140c6d60a720ecf2a0e92
Abdelrahman Soliman <bebofit@aucegypt.edu>
2023-10-18 19:20:39 +0300

    add partial refund and mint rest and ability to burn 0,1,2 to mint another one

--- M1/partial_refund.sol
+++ M1/partial_refund.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PartialRefund is ERC20 {
     address private immutable _owner;
-    uint256 constant TOTAL = 1e6 * 10 ** 18;
     error NotAuth();
+    error Forbidden(string reason);
 
     modifier onlyOwner() {
         if (msg.sender != _owner) {
@@ -15,35 +15,43 @@ contract PartialRefund is ERC20 {
         _;
     }
 
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-        uint256 tokens = initialSupply * 10 ** decimals();
-        _mint(msg.sender, tokens);
+    constructor() ERC20("Gold", "GLD") {
         _owner = msg.sender;
     }
 
     function buyToken() public payable returns (bool) {
-        require(msg.value == 1 ether, "user must pay exactly one eth");
-        require(totalSupply() < TOTAL, "sale passed, better luck next time");
+        if (msg.value != 1 ether) {
+            revert Forbidden("user must pay exactly one eth");
+        }
         uint256 tokens = 1000 * 10 ** decimals();
         if (balanceOf(address(this)) >= tokens) {
             _transfer(address(this), msg.sender, tokens);
         } else {
-            _mint(msg.sender, tokens);
+            if (balanceOf(address(this)) > 0) {
+                uint256 remainingTokens = (tokens - balanceOf(address(this)));
+                _transfer(address(this), msg.sender, balanceOf(address(this)));
+                _mint(msg.sender, remainingTokens);
+            } else {
+                _mint(msg.sender, tokens);
+            }
         }
         return true;
     }
 
     function sellBack(uint tokens) public returns (bool) {
         uint256 realTokens = tokens * 10 ** decimals();
-        require(
-            balanceOf(msg.sender) >= realTokens,
-            "cannot sellback more than what you have"
-        );
-        require(realTokens > 0, "tokens must be bigger than zero");
+        if (balanceOf(msg.sender) < realTokens) {
+            revert Forbidden("cannot sellback more than what you have");
+        }
+        if (realTokens == 0) {
+            revert Forbidden("tokens must be bigger than zero");
+        }
         _transfer(msg.sender, address(this), realTokens);
         uint256 val = (tokens * 0.5 ether);
         uint256 eth = val / 1000;
-        require(eth < address(this).balance, "cannot pay more than you have");
+        if (eth > address(this).balance) {
+            revert Forbidden("cannot pay more than you have");
+        }
         payable(msg.sender).transfer(eth);
         return true;
     }
@@ -54,7 +62,9 @@ contract PartialRefund is ERC20 {
 
     function withdraw() public onlyOwner {
         uint256 amount = address(this).balance;
-        require(amount > 0, "Nothing to withdraw; contract balance empty");
+        if (amount == 0) {
+            revert Forbidden("Nothing to withdraw; contract balance empty");
+        }
         payable(_owner).transfer(amount);
     }
 
