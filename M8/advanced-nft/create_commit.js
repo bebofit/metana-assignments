cc26576ba970bb9e7e64d741da480fe631519980
Abdelrahman Soliman <bebofit@aucegypt.edu>
2023-11-12 01:23:10 +0200

    document the contract, use randomly assigned contract to avoid collision, add commint and reveal helper to scripts

--- M8/advanced-nft/create_commit.js
+++ M8/advanced-nft/create_commit.js
@@ -4,4 +4,14 @@ const labelhash = ethers.keccak256(
   ethers.toUtf8Bytes(Math.random().toString())
 );
 
-console.log(labelhash);
+console.log("revealHash", labelhash);
+
+const commitHash = ethers.keccak256(
+  ethers.solidityPacked(
+    ["address", "bytes32"],
+    // add the contract address here
+    ["0x9a2E12340354d2532b4247da3704D2A5d73Bd189", labelhash]
+  )
+);
+
+console.log("commitHash", commitHash);
--- M8/advanced-nft/nft.sol
+++ M8/advanced-nft/nft.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "https://github.com/1001-digital/erc721-extensions/blob/main/contracts/RandomlyAssigned.sol";
 
 error AlreadyClaimed();
 error InvalidProof();
@@ -18,11 +19,18 @@ error stillEarlyToMint(uint256 blockNumber);
 error WrongHash();
 error NoMoreContributors();
 
-contract AirDropToken is ERC721 {
+/*
+ * @title AirDropToken
+ * @notice Random NFT sale
+ * @dev It is implementing ERC721 standard and commit-reveal for randomness and merkleTrees for whitelisting
+ * @author bebofit
+ */
+
+contract AirDropToken is ERC721, RandomlyAssigned {
     // bytes32 = [byte, byte, ..., byte] <- 32 bytes
     bytes32 public immutable merkleRoot;
     uint256 public mintedNfts;
-    uint256 public constant totalSupply = 1000;
+    uint256 public constant nftTotalSupply = 1000;
     address private immutable _owner;
     BitMaps.BitMap private _whiteList;
     uint256 public contributorCount;
@@ -60,12 +68,24 @@ contract AirDropToken is ERC721 {
         _;
     }
 
-    constructor(bytes32 _merkleRoot) ERC721("AirDropNFT", "ADN") {
+    /**
+     * @notice initialize contract with merkleRoot required to be done offchain
+     * @dev
+     * @param _merkleRoot bytes32
+     **/
+    constructor(
+        bytes32 _merkleRoot
+    ) ERC721("AirDropNFT", "ADN") RandomlyAssigned(nftTotalSupply, 0) {
         merkleRoot = _merkleRoot;
         _owner = msg.sender;
         contributorCount = 1;
     }
 
+    /**
+     * @notice add a contributor to the nft project only owner can do it
+     * @dev
+     * @param contributor address
+     **/
     function addContributor(address contributor) external onlyOwner {
         if (stage == Stages.SaleEnded) {
             revert NoMoreContributors();
@@ -74,6 +94,10 @@ contract AirDropToken is ERC721 {
         contributorCount++;
     }
 
+    /**
+     * @notice move project to next stage only owner can do it
+     * @dev
+     **/
     function NextStage() external onlyOwner {
         if (stage == Stages.PendingStart) {
             stage = Stages.WhiteList;
@@ -82,27 +106,40 @@ contract AirDropToken is ERC721 {
         }
     }
 
+    /**
+     * @notice allow for whitelisted addresses to commit their hash for the token
+     * @dev note that bitmap has a max of 256 addresses
+     * @param proof bytes32[] calldata
+     * @param index uint256
+     * @param commitHash bytes32
+     **/
     function claimWhiteList(
         bytes32[] calldata proof,
-        uint256 index
+        uint256 index,
+        bytes32 commitHash
     ) external atStage(Stages.WhiteList) {
-        if (stage == Stages.WhiteList) {
-            // check if already claimed
-            if (BitMaps.get(_whiteList, index)) {
-                revert AlreadyClaimed();
-            }
-
-            // verify proof
-            _verifyProof(proof, index, msg.sender);
-
-            // set airdrop as claimed
-            BitMaps.setTo(_whiteList, index, true);
+        // check if already claimed
+        if (BitMaps.get(_whiteList, index)) {
+            revert AlreadyClaimed();
         }
 
-        // mint tokens
-        _mint(msg.sender, index);
+        // verify proof
+        _verifyProof(proof, index, msg.sender);
+
+        // set airdrop as claimed
+        BitMaps.setTo(_whiteList, index, true);
+
+        // set commit to be revealed
+        _setcommit(commitHash);
     }
 
+    /**
+     * @notice verify if wallet is whitelisted or not
+     * @dev verify proof of the markle tree provided all have to be done off chain
+     * @param proof bytes32[] calldata
+     * @param index uint256
+     * @param addr address
+     **/
     function _verifyProof(
         bytes32[] memory proof,
         uint256 index,
@@ -117,20 +154,36 @@ contract AirDropToken is ERC721 {
         }
     }
 
-    function commit(bytes32 dataHash) external payable atStage(Stages.Public) {
-        if (mintedNfts == totalSupply) {
-            stage = Stages.SaleEnded;
-            revert SaleEnded();
-        }
+    /**
+     * @notice allow the public to mint using their commithash
+     * @dev
+     * @param commitHash bytes32
+     **/
+    function mint(
+        bytes32 commitHash
+    ) external payable atStage(Stages.Public) ensureAvailability {
         if (msg.value != 1 ether) {
             revert InvalidMintValue();
         }
-        commits[msg.sender].commit = dataHash;
+        _setcommit(commitHash);
+    }
+
+    function _setcommit(bytes32 commitHash) internal {
+        commits[msg.sender].commit = commitHash;
         commits[msg.sender].block = uint64(block.number);
         commits[msg.sender].revealed = false;
     }
 
-    function reveal(bytes32 revealHash) external atStage(Stages.Public) {
+    /**
+     * @notice reveal the tokenId and mint it for whitelisted or public person
+     * @dev the user must have access to the commit hash and reveal hash offchain
+     * @param revealHash bytes32
+     **/
+
+    function reveal(bytes32 revealHash) external {
+        if (stage != Stages.WhiteList && stage != Stages.Public) {
+            revert InvalidStage();
+        }
         if (commits[msg.sender].revealed) {
             revert AlreadyMinted();
         }
@@ -141,23 +194,30 @@ contract AirDropToken is ERC721 {
         if (getHash(revealHash) != commits[msg.sender].commit) {
             revert WrongHash();
         }
+        commits[msg.sender].revealed = true;
+        uint256 tokenId = nextToken(); // Create a new random token ID
 
-        bytes32 blockHash = blockhash(commits[msg.sender].block);
-        uint256 tokenId = uint256(
-            keccak256(abi.encodePacked(blockHash, revealHash))
-        ) % (totalSupply - 10);
-        // to bypass the 10 ids of whitelabled nfts
-        tokenId = tokenId + 10;
-        ++mintedNfts;
         emit Mint(tokenId);
 
         _mint(msg.sender, tokenId);
     }
 
+    /**
+     * @notice get commit hash from the data
+     * @dev
+     * @param data bytes32
+     **/
+
     function getHash(bytes32 data) public view returns (bytes32) {
         return keccak256(abi.encodePacked(address(this), data));
     }
 
+    /**
+     * @notice allow for transferring multiple nfts in one transaction
+
+     * @param addresses address[] calldata
+     * @param tokenIds uint256[] calldata
+     **/
     function bulkTransfer(
         address[] calldata addresses,
         uint256[] calldata tokenIds
@@ -166,11 +226,16 @@ contract AirDropToken is ERC721 {
             revert InvalidLength();
         }
 
-        for (uint i; i < addresses.length; i++) {
+        for (uint256 i; i < addresses.length; i++) {
             _transfer(msg.sender, addresses[i], tokenIds[i]);
         }
         return true;
     }
+
+    /**
+     * @notice at the last stage of sale allow for the contributers to share the ether equally
+     * @dev contributor count cannt be zero since owner is a contributor
+     **/
 
     function withDraw() external atStage(Stages.SaleEnded) {
         // can only withdraw when sales end;
