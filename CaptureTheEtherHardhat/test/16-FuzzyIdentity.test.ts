b1eb8b908403dfde593ca392f7efe9c4731a02c8
Abdelrahman Soliman <bebofit@aucegypt.edu>
2023-11-29 21:43:25 +0200

    solve capture the eth: public key and fuzzy identity

--- CaptureTheEtherHardhat/test/16-FuzzyIdentity.test.ts
+++ CaptureTheEtherHardhat/test/16-FuzzyIdentity.test.ts
@@ -22,6 +22,25 @@ describe('FuzzyIdentityChallenge', () => {
     /**
      * YOUR CODE HERE
      * */
+    // const wallet = getWallet();
+    const wallet = new Wallet(
+      '0xd9049714b21da5008b14de9ebe26051f79cab7025b3aba800a6a7fc4f4267973',
+      attacker.provider
+    );
+
+    let tx;
+    tx = await attacker.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther('0.1'),
+    });
+    await tx.wait();
+
+    const attackFactory = await ethers.getContractFactory('FuzzyIdentityAttack');
+    const attackContract = await attackFactory.connect(wallet).deploy(target.address);
+    await attackContract.deployed();
+
+    tx = await attackContract.attack();
+    await tx.wait();
 
     expect(await target.isComplete()).to.equal(true);
   });
--- CaptureTheEtherHardhat/test/17-PublicKey.test.ts
+++ CaptureTheEtherHardhat/test/17-PublicKey.test.ts
@@ -22,7 +22,9 @@ describe('PublicKey', () => {
     /**
      * YOUR CODE HERE
      * */
-
+    await target.authenticate(
+      '0x613a8d23bd34f7e568ef4eb1f68058e77620e40079e88f705dfb258d7a06a1a0364dbe56cab53faf26137bec044efd0b07eec8703ba4a31c588d9d94c35c8db4'
+    );
     expect(await target.isComplete()).to.equal(true);
   });
 });
